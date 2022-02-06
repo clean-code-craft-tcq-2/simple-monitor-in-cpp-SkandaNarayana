0ccdbbb9c0d8ef0ef36567c6ba8b954a33fe36ac
Skanda <skanda.narayana@in.bosch.com>
2022-02-06 20:09:58 +0530

    changes in batteryIsOk function

--- batteryStatusChecker.cpp
+++ batteryStatusChecker.cpp
@@ -13,17 +13,29 @@ bool batteryIsOk(const Battery& battery) {
     const float soc = battery.getSOC();
     const float chargeRate = battery.getChargeRate();
 
-    if (!batteryParameterWithinRange(temperature, MINIMUM_TEMP, MAXIMUM_TEMP)) {
-        cout << "Temperature out of range!\n";
-        return false;
-    }
-    if (!batteryParameterWithinRange(soc, MINIMUM_SOC, MAXIMUM_SOC)) {
-        cout << "State of Charge out of range!\n";
-        return false;
-    }
-    if (!batteryParameterWithinRange(chargeRate, MINIMUM_CHARGE_RATE, MAXIMUM_CHARGE_RATE)) {
-        cout << "Charge Rate out of range!\n";
-        return false;
-    }
-    return true;
+    bool tempParamWithinRange = batteryParameterWithinRange(temperature, MINIMUM_TEMP, MAXIMUM_TEMP);
+    bool socParamWithinRange = batteryParameterWithinRange(soc, MINIMUM_SOC, MAXIMUM_SOC);
+    bool chargeRateParamWithinRange = batteryParameterWithinRange(chargeRate, MINIMUM_CHARGE_RATE, MAXIMUM_CHARGE_RATE)) {
+    if (tempParamWithinRange || socParamWithinRange || chargeRateParamWithinRange) return true;
+    return false;
 }
+
+//bool batteryIsOk(const Battery& battery) {
+//    const float temperature = battery.getBatteryTemperature();
+//    const float soc = battery.getSOC();
+//    const float chargeRate = battery.getChargeRate();
+//
+//    if (!batteryParameterWithinRange(temperature, MINIMUM_TEMP, MAXIMUM_TEMP)) {
+//        cout << "Temperature out of range!\n";
+//        return false;
+//    }
+//    if (!batteryParameterWithinRange(soc, MINIMUM_SOC, MAXIMUM_SOC)) {
+//        cout << "State of Charge out of range!\n";
+//        return false;
+//    }
+//    if (!batteryParameterWithinRange(chargeRate, MINIMUM_CHARGE_RATE, MAXIMUM_CHARGE_RATE)) {
+//        cout << "Charge Rate out of range!\n";
+//        return false;
+//    }
+//    return true;
+//}
